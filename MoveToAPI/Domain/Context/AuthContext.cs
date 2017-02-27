756858adb324b0fead167b0f440d80f664c17277
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2017-02-27 22:10:15 +0300

    init

---
+++ MoveToAPI/Domain/Context/AuthContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MoveToAPI.Domain.Context
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
